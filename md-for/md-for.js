8d17648fb13e32966006f0379e5180efdd27ffee
Md Nadeem <nadeem.nad786@gmail.com>
2024-02-20 16:10:11 +0530

    implemented cached element to boost up perfomance

--- md-for/md-for.js
+++ md-for/md-for.js
@@ -2,22 +2,22 @@ console.time('start');
 console.log('start');
 let list = [];
 for(let i=0;i<10;i++) {
-    list.push({name: "Nad", id: i, title: "Nad "+i});
+    list.push({name: "Nad", id: i, title: "Nad "+i, success: i%2==0, isActive: i%2==0});
 }
 const instance = MiniJs.create({
     todoList: list,
     todoItem: "",
     test : {users: [{fname: "john", lname: "doe", inputVal: "tesing", attrVal: "attr value"}]},
-    user: {fname: "", lname: "", attrVal: ""},
+    user: {fname: "t", lname: "te", inputVal: "test"},
 })
 console.timeEnd('start');
 document.getElementById("todolist").addEventListener("click", (e) => {
     if(instance.todoItem) {
-        instance.todoList.push(instance.todoItem);
+        instance.todoList.push({id: instance.todoItem, name: instance.todoItem, title: instance.todoItem});
     }
 });
 document.getElementById("addUser").addEventListener("click", (e) => {
     if(instance.user.fname) {
-        instance.test.users.push(instance.user);
+        instance.test.users.push({...instance.user});
     }
 })
